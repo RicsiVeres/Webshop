206a39671339cccfb535a59b065dcf018195e28e
RicsiVeres <120040566+RicsiVeres@users.noreply.github.com>
2023-04-02 18:00:48 +0200

    Add files via upload

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Wofwgwg</h1>
-      <button className="btn btn-primary btn-lg">katt</button>
-    </div>
-  );
+  return <div></div>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./Home";
-
+import Product from "./Products/product";
+import "./main.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <Home />
+    <div className="container">
+      <div id="d">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
   </div>
 );
