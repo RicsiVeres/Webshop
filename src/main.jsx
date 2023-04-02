385b96f0440d0b88b304f0590f931438df572cf2
Mészáros Sándor <128988320+MeszarosSanyi@users.noreply.github.com>
2023-04-02 18:07:34 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import Home from "./Home";
 import Product from "./Products/product";
 import "./main.css";
+import NavBar from "./NavBar/NavBar"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    <Home />
+    <NavBar />
     <div className="container">
       <div id="d">
         <Product />
